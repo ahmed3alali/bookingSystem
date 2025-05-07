e446ff7c291fa979aa257cf1e60a2700d480a5a1
Habeeb_sh <101204241+Habeeb-sh@users.noreply.github.com>
2025-05-07 20:53:56 +0300

    Update ctest.java

--- ctest.java
+++ ctest.java
@@ -14,7 +14,7 @@ public class ctest {
         // Database connection details
         String hostname = "localhost";
         String sqlInstanceName = "Habeeb\\SQLEXPRESS"; //computer name 
-        String sqlDatabase = "HRLC_DB";  //sql server database name
+        String sqlDatabase = "flight_booking_system";  //sql server database name
         String sqlUser = "sa";
         String sqlPassword = "1234"; //passwrod sa account
         // Load the SQL Server JDBC driver
@@ -39,15 +39,6 @@ public class ctest {
         }
     }
 
-    
-    
-    
-    
-    
-    
-    
-
-        
             public static void main(String[] args) throws ClassNotFoundException, SQLException {
         
         
@@ -58,27 +49,7 @@ public class ctest {
             Logger.getLogger(ctest.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        // SQL query to select all records from the employees table
-        String query = "SELECT * FROM Stock"; 
-
-        // Create a statement and execute the query
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery(query);
-
-        // Process the result set
-        while (rs.next()) {
-            // Assuming the employees table has columns 'id' and 'name'
-            int id = rs.getInt("stock_id");
-            String name = rs.getString("date");
-
-            System.out.println("ID: " + id + ", Name: " + name); // Print the results
-        }
-
-        // Close the resources
-        rs.close();
-        stmt.close();
         conn.close();
     }
     
 }
-
